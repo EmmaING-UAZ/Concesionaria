672a7607790e20d7d8af031ea24367958b122f3e
EmmaING-UAZ <tsurutun3ad0@Kali.ipv6.infinitum.net.mx>
2023-03-07 16:08:59 -0600

    Conserionaria Actualizada

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ class Program
     {
         Concesionaria concesionaria = new Concesionaria(3);
 
-        Automovil auto1 = new Automovil { ID = 1, Marca = "Toyota", Km = 10000, Precio = 20000 };
-        Automovil auto2 = new Automovil { ID = 2, Marca = "Ford", Km = 5000, Precio = 15000 };
-        Automovil auto3 = new Automovil { ID = 3, Marca = "Chevrolet", Km = 8000, Precio = 18000 };
+        Automovil auto1 = new Automovil { ID = 1, Marca = "Ford", Km = 10000, Precio = 20000 };
+        Automovil auto2 = new Automovil { ID = 2, Marca = "Volkswagen", Km = 5000, Precio = 50000 };
+        Automovil auto3 = new Automovil { ID = 3, Marca = "Audi", Km = 3000, Precio = 500000 };
 
         concesionaria.AgregarAuto(auto1);
         concesionaria.AgregarAuto(auto2);
