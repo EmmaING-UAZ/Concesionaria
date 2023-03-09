1ea9d606f3fa75ba780f5eeb6a9f02ef33a642d2
EmmaING-UAZ <tsurutun3ad0@Kali.ipv6.infinitum.net.mx>
2023-03-09 15:51:51 -0600

    Actualizacion 9 de marzo

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        bool MostrarMenu = true;
+
+        while (MostrarMenu){
+            MostrarMenu = Menu();
+        }
+
         Concesionaria concesionaria = new Concesionaria(3);
 
         Automovil auto1 = new Automovil { ID = 1, Marca = "Ford", Km = 10000, Precio = 20000 };
@@ -18,4 +25,54 @@ class Program
 
     }
 
+    public static bool Menu(){
+        
+        Console.Clear;
+        Console.WriteLine("Seleccione una opción");
+        Console.WriteLine("1.- Agregar Automovil");
+        Console.WriteLine("1.- Agregar Automovil");
+        Console.WriteLine("1.- Agregar Automovil");
+        Console.WriteLine("1.- Agregar Automovil");
+        Console.WriteLine("1.- Agregar Automovil");
+        Console.WriteLine("1.- Agregar Automovil");
+        Console.WriteLine("Opción");
+
+        switch(Console.ReadLine())
+        {
+             case "1":
+                Console.WriteLine("Seleccionaste 1");
+                Console.ReadLine();
+                return true;
+
+            case "2":
+                Console.WriteLine("Seleccionaste 2");
+                Console.ReadLine();
+                return true;
+
+            case "3":
+                Console.WriteLine("Seleccionaste 3");
+                Console.ReadLine();
+                return true;
+
+            case "4":
+                Console.WriteLine("Seleccionaste 4");
+                Console.ReadLine();
+                return true;
+
+            case "5":
+                return false;
+            default:
+                return true;
+
+        }
+        
+
+
+
+        
+    }
+
+
+
+    
 }
